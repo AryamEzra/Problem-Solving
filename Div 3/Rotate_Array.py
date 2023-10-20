86e35305bed6c2bbfd276d7a263a587493493544
AryamEzra <aryamiyezra@gmail.com>
2023-10-21 01:01:24 +0300

    Rotate array

---
+++ Div 3/Rotate_Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        
+        #reverse the array
+        l,r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        # reverse the 1st k elements 
+        l,r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        #reverse the remaining elements after that
+        l,r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+        #time complexity: o(n)
+        #space complexty: o(1)
