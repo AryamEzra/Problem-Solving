4ba9d1396a4f346ff475cbdbe27c4414e8aaf3ed
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2023-11-30 18:41:45 +0300

    Added solution for Number of Good Pairs

---
+++ A2SV/number-of-good-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        pair = 0
+        left = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    pair += 1
+        return pair
+        
