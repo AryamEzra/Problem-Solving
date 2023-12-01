f4d77873fbfd118bb71f9d0db6c1bd657f054cfc
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2023-12-01 20:00:51 +0300

    Added solution for Merge Strings Alternately

---
+++ A2SV/merge-strings-alternately.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0
+        j = 0
+        output = []
+
+        while i < len(word1) and j < len(word2):
+            output.append(word1[i])
+            output.append(word2[j])
+            i += 1
+            j += 1
+
+        if len(word1) < len(word2):
+            output.append(word2[j:])
+        elif len(word2) < len(word1):
+            output.append(word1[i:])
+        
+        return "".join(output)
+
+        
