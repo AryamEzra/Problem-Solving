59d69369f269f6f0831282442df46207089bb6a5
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2023-12-06 16:32:02 +0300

    Added solution for Non-decreasing Array

---
+++ A2SV/non-decreasing-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        modify = 0
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                if modify > 0:
+                    return False
+                modify += 1
+                if i == 0 or nums[i-1] <= nums[i+1]:
+                    nums[i] == nums[i+1]
+                else:
+                    nums[i+1] = nums[i]
+        return True
+        
