50f6256b3d17a037df34788b2fd8097a1f44dbe0
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2023-12-07 16:02:14 +0300

    Added solution for Partition Array According to Given Pivot

---
+++ A2SV/partition-array-according-to-given-pivot.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        before = []
+        after = []
+        equal = []
+
+        for i in range(len(nums)):
+            if nums[i] < pivot:
+                before.append(nums[i])
+            elif nums[i] == pivot:
+                equal.append(nums[i])
+            else:
+                after.append(nums[i])
+        return before + equal + after
+
+
+
+        
