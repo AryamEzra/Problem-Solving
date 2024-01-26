07001ec5204397ee684e8095ee23aee7ab2b34fd
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-01-26 17:48:59 +0300

    Add solution for Odd Even Linked List

--- A2SV/leetcode/odd-even-linked-list.py
+++ A2SV/leetcode/odd-even-linked-list.py
@@ -6,30 +6,21 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #we are not checking for the values inside of them reather the positions at which they are odd or even
-        #if it has no elem, then only 1 odd elem, then 2 ele odd and even then just return them as is
-        if not head or not head.next or not head.next.next:
-            return head
-
+        if head == None:
+            return 
         odd = head
-        cur = head
-        even = head.next
-        start_even = head.next
-        
-        i = 1
+        even = odd.next
 
-        while cur:
-            #here i > 2 becuase we already reserved odd and first as the first two  
-            if i > 2 and i % 2 == 1:
-                odd.next = cur
-                odd = odd.next
-            elif i > 2 and i % 2 == 0:
-                even.next = cur
-                even = even.next
-            cur = cur.next 
-            i += 1
-        even.next = None #the even should be none
-        odd.next = start_even # the odd should point twoards the head of the even in the end
+        start_even = even
+        while even is not None and even.next is not None:
+            odd.next = even.next #1->3
+            odd = odd.next # the value we had just arrived the 3 is not the odd
+            even.next = odd.next #2->4 beacuse we were at 3 and the value next to 3 is 4 thereforew e should set 2 to 4
+            even = even.next # our prevoius value was 2 and now we set that to equal to 4 then since the loop will end having the evn point to none then finally make the odd's tail become the even's
+        odd.next = start_even
         return head
+        
+        
 
 
         
