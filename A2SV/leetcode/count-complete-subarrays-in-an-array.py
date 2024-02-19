305742d9136c6158e57d9254c18f492d6942651e
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-02-19 16:26:09 +0300

    Add solution for Count Complete Subarrays in an Array

--- A2SV/leetcode/count-complete-subarrays-in-an-array.py
+++ A2SV/leetcode/count-complete-subarrays-in-an-array.py
@@ -1,17 +1,19 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        #time: O(n**2)
+        #time: O(n)
         #space: O(n)
 
-        n = len(nums)
-        s = len(set(nums))
-        count = 0
-        for i in range(n):
-            seen = set()
-            for j in range(i,n):
-                seen.add(nums[j])
-                if len(seen) == s:
-                    count += 1
-                elif len(seen) > s:
-                    break
-        return count
+         
+        set_nums = len(set(nums))
+        ans = 0
+        seen = defaultdict(int)
+        left = 0
+        for right in range(len(nums)):
+            seen[nums[right]] +=1
+            while len(seen) == set_nums :
+                ans += len(nums) - right
+                seen[nums[left]] -=1
+                if not seen[nums[left]]:
+                    del seen[nums[left]]
+                left +=1
+        return ans
