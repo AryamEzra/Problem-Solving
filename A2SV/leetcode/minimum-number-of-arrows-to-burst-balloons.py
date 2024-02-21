ba9cbe89469fff2c1286a5c4d28f7dcd283db27c
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-02-21 11:09:39 +0300

    Add solution for Minimum Number of Arrows to Burst Balloons

--- A2SV/leetcode/minimum-number-of-arrows-to-burst-balloons.py
+++ A2SV/leetcode/minimum-number-of-arrows-to-burst-balloons.py
@@ -3,7 +3,7 @@ class Solution:
         arrows = 0 
         points.sort()
         minn = float('-inf')
-        print(points)
+    
         for i in range(len(points)):
             if points[i][0] <= minn and minn <= points[i][1]:
                 minn = min(minn, points[i][1])
