01ebf774d19ef9714acc2344815e9210d25499b0
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-03-01 17:57:17 +0300

    Add solution for Predict the Winner

--- A2SV/leetcode/predict-the-winner.py
+++ A2SV/leetcode/predict-the-winner.py
@@ -1,10 +1,6 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        
-
-
-        p1 = [0, 0]  # left_score
-        p2 = [0, 0] # right_score
+       
         def scoreCalc(s, e, turn):
             if s == e:
                 if turn:
@@ -38,44 +34,3 @@ class Solution:
         var =  scoreCalc(0, len(nums)-1,True)
         return var[0] >= var[1]
 
-
-
-
-        #(pl-(p2-(pl-(p2-(pl-(p2-(pl-))))))) - onw score approch
-        # def solve(left, right):
-        #     if left == right:
-        #         return nums[left]
-        #     left_score = nums[left] - solve(left + 1, right)
-        #     right_score = nums[right] - solve(left, right - 1)
-
-        #     return left_score >= right_score
-        # return solve(0, len(nums) - 1) >= 0
-
-        # def scoreCalc(s, e, turn):
-        #     if s == e:
-        #         if turn:
-        #             return [nums[s], 0]
-        #         return [0, nums[s]]
-            
-        #     score = [0, 0]
-
-        #     if turn:
-        #         temp = scoreCalc(s + 1, e, not turn)
-        #         temp2 = scoreCalc(s, e - 1, not turn)
-
-        #         score[0] += max(nums[s] + temp[0], temp2[1] + nums[e])
-        #         print(temp)
-        #         print(temp2) 
-        #     else:
-        #         temp = scoreCalc(s + 1, e, not turn)
-        #         temp2 = scoreCalc(s, e - 1, not turn)
-
-        #         score[1] += max(nums[s] + temp[0], temp2[1] + nums[e])
-        #         print(temp)
-        #         print(temp2)  
-            
-        #     return score
-
-        # val = scoreCalc(0, len(nums)-1,True)
-        
-        # return val[1] <= val[0]
