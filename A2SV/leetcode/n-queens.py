967b49c42e4c74e15bb6f237eac9a7b3e28537ed
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-03-08 21:37:47 +0300

    Add solution for N-Queens

---
+++ A2SV/leetcode/n-queens.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set()
+        right_diagonal = set()
+        left_diagonal = set()
+        rowy = []
+        board = [["." for _ in range(n)] for _ in range(n)]
+        print(board)
+        def chess(row):
+            if row == n:
+                fake =[]
+                for i in board:
+                    fake.append("".join(i))
+                rowy.append(fake)
+                return
+            for i in range(n):
+                if i in cols or i+row in right_diagonal or row-i in left_diagonal:
+                    continue
+                board[row][i] = "Q"
+                cols.add(i)
+                right_diagonal.add(row+i)
+                left_diagonal.add(row-i)
+
+                chess(row + 1)
+
+                board[row][i] = "."
+                print(cols)
+                cols.remove(i)
+                right_diagonal.remove(row+i)
+                left_diagonal.remove(row-i)
+                
+        chess(0)
+        return rowy
