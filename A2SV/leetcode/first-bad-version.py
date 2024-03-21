df497f4afda34b93338d4bc72b8b0649bddf163f
Aryam Ezra <120574560+AryamEzra@users.noreply.github.com>
2024-03-21 22:30:12 +0300

    Add solution for First Bad Version

--- A2SV/leetcode/first-bad-version.py
+++ A2SV/leetcode/first-bad-version.py
@@ -5,10 +5,11 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l = 1
         r = n
+        # ans was previusly n 
         while l <= r:
             mid = l + (r-l) // 2
             if isBadVersion(mid): # return True -  bad
-                ans = mid
+                # ans = mid
                 r = mid - 1
             else: # return false - not bad
                 l = mid + 1
